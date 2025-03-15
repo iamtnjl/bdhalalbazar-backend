2d3a406842727f5fc201e474e26f63da0b4d1c14
tanjil-repliq <md.tanjil@repliq.dev>
2025-03-15 15:32:34 +0600

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,8 @@ git pull origin main
  
 echo "Installing Dependencies..."
 npm install --yes
+
+echo "Restarting Passenger..."
+touch tmp/restart.txt || { echo "Error: Failed to restart Passenger"; exit 1; }
  
 echo "Deployment Completed Successfully!"
