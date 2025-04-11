7639217dba5bd27b8e86a3c6e39f3fb705148c1e
iamtnjl <md.tanjil@repliq.dev>
2025-04-11 18:47:12 +0600

    Increase token expried date

--- src/utils/generateTokens.js
+++ src/utils/generateTokens.js
@@ -4,13 +4,13 @@ const generateTokens = (user) => {
   const accessToken = jwt.sign(
     { userId: user._id, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: "15m" } // Access token valid for 15 minutes
+    { expiresIn: "7d" }
   );
 
   const refreshToken = jwt.sign(
     { userId: user._id },
     process.env.REFRESH_SECRET,
-    { expiresIn: "7d" } // Refresh token valid for 7 days
+    { expiresIn: "14d" }
   );
 
   return { accessToken, refreshToken };
